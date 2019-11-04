f1603b5aa9824548a822c1a972bb98059229776d
Lorena <lorena.tacal@gmail.com>
2019-11-04 14:04:45 +0000

    Start the game only when the user is adding his name

--- src/App.js
+++ src/App.js
@@ -3,20 +3,39 @@ import styled from 'styled-components';
 import Board from './components/Board';
 import UserName from './components/UserName'
 
+const StyledDiv = styled.div`
+  margin: 2% 14%;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  border-radius: 5px;
+`
+
 const StyledTitle = styled.h1`
   margin: 2%;
   text-align: center;
   font-Family: Cantarell, italic
+  font-color: white
 `
 
 function App() {
+  const [name, setName] = React.useState('');
 
+  
   return (
-    <div>
+  <>
+    <StyledDiv>
       <StyledTitle>Match the Pair</StyledTitle>
-      <UserName />
-      <Board />
+      <UserName name={name} setName={setName}/>
+    </StyledDiv>
+
+    <div>
+    {
+      (name !== '')
+      ? <Board />
+      : null
+    }
     </div>
+  </>
   );
 }
 
--- src/components/UserName.js
+++ src/components/UserName.js
@@ -6,15 +6,22 @@ const StyledTitle = styled.h1`
   font-Family: Cantarell, italic
 `
 const StyledInput= styled.input`
-    margin-left: 40%;
+    margin-left: 38%;
+    margin-bottom: 2%;
     font-size: 1em;
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    transition: 0.3s;
+    border-radius: 5px;
 `
 const StyledButton= styled.button`
-    font-size: 1em;
+  font-size: 1em;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  border-radius: 5px;
 `
 
-function UserName(props) {
-    const [name, setName] = React.useState('Player')
+function UserName({name, setName}) {
+    //const [name, setName] = React.useState('Player')
     const [typedInput, setTypedInput] = React.useState('')
     
     return(
